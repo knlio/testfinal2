e5529b74d481730445cb577e484021858d2db7fb
knlio <128051458+knlio@users.noreply.github.com>
2023-09-20 13:05:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ import os
 # Set the OpenAI API key
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
+def get_completion_from_messages(messages, model="gpt-3.5-turbo"):
+  """
+  Get a completion from the OpenAI API.
+
+  Args:
+    messages: A list of messages in the conversation.
+    model: The OpenAI model to use.
+
+  Returns:
+    The completion from the OpenAI API.
+  """
+
+  response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages,
+    temperature=0, # this is the degree of randomness of the model's output
+  )
+  return response.choices[0].message["content"]
 
 context = [ {'role':'system', 'content':"""
 You are a personal trainer named ماي كوتش, an automated service to works as a personal trainer and provides personalized weekly meal plans, workouts routines and motivation for each user based on their goals, preferences, and other factors. to help people get healthy and in shape with a monthly subscription system. \
@@ -22,7 +32,7 @@ You wait to collect the entire users data , then summarize it and check for a fi
 time if the customer wants to add anything else. \
 Finally you collect the payment.\
 you will send Daily motivational messages to keep users motivated and on track.\
-you will send Daily  Reminders to complete their workouts and follow their meal plans .\
+you will send Daily Reminders to complete their workouts and follow their meal plans .\
 Progress tracking and reporting to help users see their progress over time . \
 Customize the conversation for each user based on collect data from users that include the information given in the questions that were asked by you.\
 the subscription price is 50 Saudi riyals per month .\
@@ -31,7 +41,7 @@ Make sure to clarify all options \
 identify the info from the list.\
 weekly meal plans, workouts routines and motivation based form choose from these 3 goals they want to reach . \
 You respond in a short and concise , very conversational no profanity always happy and friendly style. \
-The user data  includes information such as  \
+The user data includes information such as \
 user name \
 age \
 gender \
@@ -42,16 +52,16 @@ have any dietary restrictions (e.g. allergies, preferences, etc.) \
 fitness level (e.g. beginner, intermediate, advanced) \
 fitness goals (e.g. lose weight, gain muscle, improve overall fitness) \
 
-"""} ]  # accumulate messages
+"""} ] # accumulate messages
 
 
 inp = st.text_input(label="Enter text here…", value="Hi")
 button_conversation = st.button(label="Chat!")
 
 if button_conversation:
-    prompt = inp
-    inp.value = ''
-    context.append({'role':'user', 'content':f"{prompt}"})
-    response = get_completion_from_messages(context)
-    context.append({'role':'assistant', 'content':f"{response}"})
-    st.write(f"Assistant: {response}")
+  prompt = inp
+  inp = ''
+  context.append({'role':'user', 'content':f"{prompt}"})
+  response = get_completion_from_messages(context)
+  context.append({'role':'assistant', 'content':f"{response}"})
+  st.write(f"Assistant: {response}"))
